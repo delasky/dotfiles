07ed95c770b181ad82be4469020fc7747fbcc688
Daniel Olasky <daniel.olasky@hotschedules.com>
2018-02-27 10:17:44 -0800

    ctags

--- install.sh
+++ install.sh
@@ -13,6 +13,7 @@ ln -sv "$DOTFILES_DIR/.gitconfig" ~
 ln -sv "$DOTFILES_DIR/.gitignore_global" ~
 ln -sv "$DOTFILES_DIR/.npmrc" ~
 ln -sv "$DOTFILES_DIR/.profile" ~
+ln -sv "$DOTFILES_DIR/.ctags" ~
 ln -sv "$DOTFILES_DIR/.vimrc" ~
 ln -sv "$DOTFILES_DIR/.tmux.conf" ~
 ln -sv "$DOTFILES_DIR/.hledger.journal" ~
